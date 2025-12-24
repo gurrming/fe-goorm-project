ec04ae443f0da09fb49583a41611135044efd43c
정찬영 <q135794268020@gmail.com>
2025-12-24 16:24:41 +0900

    setting: 페이지 router 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import Layout from './components/layout/Layout';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
