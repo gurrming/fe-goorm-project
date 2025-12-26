99c5d33fd9c0b3c3464b3d119fda7bce2784c2ba
202110861 <dbsalsk2514@naver.com>
2025-12-26 18:49:31 +0900

    feat: 보유자산 목록 추가

---
+++ src/components/right/contents/asset/mockData.ts
@@ -0,0 +1,10 @@
+export const mockAssetData = [
+  {
+    asset: '비트코인',
+    quantity: 0.0015653,
+    averagePrice: 5954000,
+    purchaseAmount: 101000,
+    evaluationAmount: 29500000,
+    profitAndLoss: 2950000,
+  },
+];
