ffdd23e439fc87622ebff90789a4b69f269dd6d1
202110861 <dbsalsk2514@naver.com>
2025-12-26 10:42:16 +0900

    feat: 버튼 컴포넌트 제작

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,3 +1,26 @@
-export default function Button() {
-  return <div>Button</div>;
+import type { ButtonHTMLAttributes } from 'react';
+import { cn } from '../../lib/utils';
+
+interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  colorType: 'white' | 'blue' | 'indigo' | 'gray' | 'red';
+}
+
+export default function Button({ children, colorType, onClick, ...props }: IButtonProps) {
+  const colorStyle = {
+    white: 'bg-[#EDEEF1] text-primary-100',
+    blue: 'bg-[#0062DF] text-white',
+    indigo: 'bg-[#0C3887] text-white',
+    gray: 'bg-[#999999] text-white',
+    red: 'bg-[#DD3C44] text-white',
+  };
+  return (
+    <button
+      className={cn('rounded-md px-4 py-2 w-full', colorStyle[colorType], 'hover:cursor-pointer')}
+      onClick={onClick}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 }
