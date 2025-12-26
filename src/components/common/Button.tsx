cff07072e60dcbcc2ad7b448fb27a4f38dd28864
202110861 <dbsalsk2514@naver.com>
2025-12-26 10:53:59 +0900

    feat : jsdocs 추가

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,11 +1,18 @@
 import type { ButtonHTMLAttributes } from 'react';
 import { cn } from '../../lib/utils';
 
+/**
+ * Button Component
+ *
+ * Button의 색상은 colorType에 따라 결정됩니다.
+ *
+ * @param children - Button의 내용
+ * @param colorType - Button의 색상 (white, blue, indigo, gray, red)
+ */
 interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
   colorType: 'white' | 'blue' | 'indigo' | 'gray' | 'red';
 }
-
 export default function Button({ children, colorType, onClick, ...props }: IButtonProps) {
   const colorStyle = {
     white: 'bg-[#EDEEF1] text-primary-100',
