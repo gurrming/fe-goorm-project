aaa2622a10ce9641e77c66f4bc2f140142d50dbe
202110861 <dbsalsk2514@naver.com>
2025-12-26 18:36:45 +0900

    fix: thead 글자 색 변경

--- src/components/right/contents/transaction/settled.tsx
+++ src/components/right/contents/transaction/settled.tsx
@@ -5,19 +5,19 @@ const Settled = () => {
     <table className="w-full border-collapse bg-white">
       <thead>
         <tr className="border-b border-t border-gray-200 bg-[#F7F7F7]">
-          <th className="px-4 py-3 text-center text-[11px] font-medium text-[#333333] border-r border-gray-200">
+          <th className="px-4 py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
             주문시간
           </th>
-          <th className="px-4 py-3 text-center text-[11px] font-medium text-[#333333] border-r border-gray-200">
+          <th className="px-4 py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
             구분
           </th>
-          <th className="px-4 py-3 text-center text-[11px] font-medium text-[#333333] border-r border-gray-200">
+          <th className="px-4 py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
             <div className="flex flex-col">
               <span>체결가격</span>
-              <span className="text-[11px] font-normal text-[#333333] mt-1">체결금액</span>
+              <span className="text-[11px] font-normal text-[#666666] mt-1">체결금액</span>
             </div>
           </th>
-          <th className="px-4 py-3 text-center text-[11px] font-medium text-[#333333]">체결수량</th>
+          <th className="px-4 py-3 text-center text-[11px] font-medium text-[#666666]">체결수량</th>
         </tr>
       </thead>
       <tbody>
--- src/components/right/contents/transaction/unSettled.tsx
+++ src/components/right/contents/transaction/unSettled.tsx
@@ -12,25 +12,25 @@ const UnSettled = () => {
       <table className="w-full border-collapse bg-white">
         <thead>
           <tr className="border-b border-t border-gray-200 bg-[#F7F7F7]">
-            <th className="px-4 py-3 text-center text-[11px] font-medium text-[#333333] border-r border-gray-200">
+            <th className="px-4 py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
               주문시간
             </th>
-            <th className="px-4 py-3 text-center text-[11px] font-medium text-[#333333] border-r border-gray-200">
+            <th className="px-4 py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
               구분
             </th>
-            <th className="px-4 py-3 text-center text-[11px] font-medium text-[#333333] border-r border-gray-200">
+            <th className="px-4 py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
               <div className="flex flex-col">
                 <span>감시가격</span>
-                <span className="text-[11px] font-normal text-[#333333] mt-1">주문금액</span>
+                <span className="text-[11px] font-normal text-[#666666] mt-1">주문금액</span>
               </div>
             </th>
-            <th className="px-4 py-3 text-center text-[11px] font-medium text-[#333333] border-r border-gray-200">
+            <th className="px-4 py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
               <div className="flex flex-col">
                 <span>주문량</span>
-                <span className="text-[11px] font-normal text-[#333333] mt-1">미체결량</span>
+                <span className="text-[11px] font-normal text-[#666666] mt-1">미체결량</span>
               </div>
             </th>
-            <th className="px-4 py-3 text-center text-[11px] font-medium text-[#333333] ">주문취소</th>
+            <th className="px-4 py-3 text-center text-[11px] font-medium text-[#666666] ">주문취소</th>
           </tr>
         </thead>
         <tbody>
