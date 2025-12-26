adc488ed8d464e2e8fb7626990f9df542a59251c
202110861 <dbsalsk2514@naver.com>
2025-12-26 17:54:47 +0900

    feat: 보유자산 추가

--- src/components/right/contents/asset/asset.tsx
+++ src/components/right/contents/asset/asset.tsx
@@ -1,5 +1,13 @@
+import AssetList from './assetList';
+import MyAsset from './myAsset';
+
 const Asset = () => {
-  return <div>Asset</div>;
+  return (
+    <div className="w-full flex flex-col gap-3">
+      <MyAsset />
+      <AssetList />
+    </div>
+  );
 };
 
 export default Asset;
