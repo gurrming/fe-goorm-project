fd3789d8c919f5ba0168a5b1b441b19f26c6f36e
202110861 <dbsalsk2514@naver.com>
2025-12-30 14:25:05 +0900

    fix: 종목 정보 UI 변경

--- src/components/chart/PriceInfo.tsx
+++ src/components/chart/PriceInfo.tsx
@@ -5,12 +5,12 @@ import type { TDayData } from '../../types/upBit';
 const PriceInfo = ({ data }: { data: TDayData[] }) => {
   console.log(data);
   const MARKET = data?.[0]?.market;
-  const UNIT = MARKET.split('-')[1];
-  const QUOTE = MARKET.split('-')[0];
+  const UNIT = MARKET?.split('-')[1];
+  const QUOTE = MARKET?.split('-')[0];
 
   return (
-    <div className="flex justify-between items-center">
-      <div className="flex flex-col px-4">
+    <div className="flex items-center my-2">
+      <div className="flex flex-col px-4 w-1/2">
         <p className={`text-[20px] font-bold ${data?.[0]?.change_rate < 0 ? 'text-[#0062DF]' : 'text-[#DD3C44]'}`}>
           {data?.[0]?.trade_price.toLocaleString('ko-KR')} <span className="text-sm">{QUOTE}</span>
         </p>
@@ -23,28 +23,18 @@ const PriceInfo = ({ data }: { data: TDayData[] }) => {
           </p>
         </div>
       </div>
-      <div className="flex gap-2 px-4 py-2">
-        <div className="flex flex-col">
-          <Text
-            size="xs"
-            text="고가"
-            price={data?.[0]?.high_price.toLocaleString('ko-KR')}
-            minus={data?.[0]?.change_rate < 0 ? true : false}
-          />
+      <div className="flex justify-end gap-2 px-4 py-2 w-1/2">
+        <div className="flex flex-col gap-2 w-full">
+          <Text size="xs" text="고가" price={data?.[0]?.high_price.toLocaleString('ko-KR')} priceColor="red" />
           <div className="h-[0.5px] bg-gray-200 w-full" />
-          <Text
-            size="xs"
-            text="저가"
-            price={data?.[0]?.low_price.toLocaleString('ko-KR')}
-            minus={data?.[0]?.change_rate < 0 ? true : false}
-          />
+          <Text size="xs" text="저가" price={data?.[0]?.low_price.toLocaleString('ko-KR')} priceColor="blue" />
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col gap-2 w-full">
           <Text
             size="xs"
             text="거래량"
             price={data?.[0]?.candle_acc_trade_volume.toLocaleString('ko-KR')}
-            minus={false}
+            priceColor="black"
             type={UNIT}
           />
           <div className="h-[0.5px] bg-gray-200 w-full" />
@@ -52,7 +42,7 @@ const PriceInfo = ({ data }: { data: TDayData[] }) => {
             size="xs"
             text="거래대금"
             price={data?.[0]?.candle_acc_trade_price.toLocaleString('ko-KR')}
-            minus={false}
+            priceColor="black"
             type={QUOTE}
           />
         </div>
