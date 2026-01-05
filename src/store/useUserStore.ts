e625230f795ca90c80855df231d6387de730fc4c
202110861 <dbsalsk2514@naver.com>
2026-01-05 12:31:25 +0900

    fix: 로그인 성공시 받는 데이터 중 id, nickname 을 전역으로 저장

--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -2,17 +2,21 @@ import { create } from 'zustand';
 import { postLogout } from '../api/member';
 
 interface IUserStore {
-  user: string | null;
-  setUser: (user: string) => void;
+  user: {
+    id: number;
+    nickname: string;
+  } | null;
+  setUser: (user: { id: number; nickname: string }) => void;
   logout: () => void;
 }
 
 const useUserStore = create<IUserStore>((set) => ({
   user: null,
-  setUser: (user) => set({ user }),
-  logout: () => {
+  setUser: (user) => set({ user: { id: user.id, nickname: user.nickname } }),
+  logout: async () => {
     localStorage.removeItem('accessToken');
-    postLogout();
+    localStorage.removeItem('refreshToken');
+    await postLogout();
     set({ user: null });
   },
 }));
