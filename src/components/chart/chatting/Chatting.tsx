ae6b18a8b96b9f255684a67342be493cf4b2b344
202110861 <dbsalsk2514@naver.com>
2026-01-05 19:08:32 +0900

    fix: 채팅 파일에 웹소켓 연결

--- src/components/chart/chatting/Chatting.tsx
+++ src/components/chart/chatting/Chatting.tsx
@@ -1,30 +1,41 @@
 import React, { useState } from 'react';
 import Chat from './Chat';
-import { mockChatData } from './mockData';
+import { useChatting } from '../../../hooks/useChatting';
 
 const Chatting = () => {
   const [message, setMessage] = useState('');
   const [isComposing, setIsComposing] = useState(false);
 
+  const { isConnected, sendChat, chatHistory } = useChatting({ categoryId: 1 });
+
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter' && message.trim() !== '' && !isComposing) {
-      setMessage((e.target as HTMLInputElement).value);
+    if (e.key === 'Enter' && message.trim() !== '' && !isComposing && isConnected) {
+      sendChat(message);
       setMessage('');
     }
   };
+
   return (
     <div className="flex flex-col gap-2 w-[1000px] p-4">
-      {mockChatData.map((chat, index) => {
-        const prevChat = index > 0 ? mockChatData[index - 1] : null;
+      {chatHistory.map((chat, index) => {
+        const prevChat = index > 0 ? chatHistory[index - 1] : null;
         let hideDay = false;
-        const sendDay = chat.time.split('T')[0];
+        const sendDay = chat.chatTime.split('T')[0];
 
         if (prevChat) {
           // 날짜가 다르면 hideDay = false (날짜 표시), 같으면 hideDay = true (날짜 숨김)
-          hideDay = sendDay === prevChat.time.split('T')[0];
+          hideDay = sendDay === prevChat.chatTime.split('T')[0];
         }
 
-        return <Chat key={index} {...chat} hideDay={hideDay} />;
+        return (
+          <Chat
+            key={index}
+            nickname={chat.memberNickname}
+            message={chat.chatContent}
+            time={chat.chatTime}
+            hideDay={hideDay}
+          />
+        );
       })}
       <input
         type="text"
