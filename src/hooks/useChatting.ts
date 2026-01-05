2d1baa62aae3762aff25427986c59662f222fe2f
202110861 <dbsalsk2514@naver.com>
2026-01-05 19:05:53 +0900

    feat: websocket topic 구독 및 채팅 전송 기능 구현

---
+++ src/hooks/useChatting.ts
@@ -0,0 +1,50 @@
+import { useCallback, useEffect, useState } from 'react';
+import { useWebsocket } from './useWebsocket';
+import { useGetChatList } from '../api/useGetChatList';
+import type { TChat } from '../types/chat';
+
+export const useChatting = ({ categoryId }: { categoryId: number }) => {
+  const { isConnected, stompClientRef, connect } = useWebsocket();
+  const [chatHistory, setChatHistory] = useState<TChat[]>([]);
+
+  useEffect(() => {
+    const { data: chatList, refetch } = useGetChatList(categoryId);
+    setChatHistory(chatList || []);
+    refetch();
+  }, [categoryId]);
+
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      connect(token);
+    }
+  }, [connect]);
+
+  useEffect(() => {
+    if (isConnected && stompClientRef.current) {
+      const subscription = stompClientRef.current.subscribe(`/topic/chat/${categoryId}`, (message) => {
+        const body = JSON.parse(message.body);
+        console.log(JSON.stringify(body));
+        setChatHistory((prev) => [...prev, body]);
+      });
+
+      return () => {
+        subscription.unsubscribe();
+      };
+    }
+  }, [isConnected, stompClientRef, categoryId]);
+
+  const sendChat = useCallback(
+    (message: string) => {
+      if (stompClientRef.current?.active) {
+        stompClientRef.current.publish({
+          destination: `/app/chat/${categoryId}`,
+          body: JSON.stringify({ message }),
+        });
+      }
+    },
+    [categoryId, stompClientRef],
+  );
+
+  return { isConnected, sendChat, chatHistory };
+};
