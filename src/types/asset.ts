2c72610f1d2e7bae30cc4f8e234081d607cc039a
202110861 <dbsalsk2514@naver.com>
2026-01-07 14:04:56 +0900

    feat: 내 자산 목록 타입 추가 및 api 연결

---
+++ src/types/asset.ts
@@ -0,0 +1,26 @@
+export type TMyAsset = {
+  assetCash: number;
+  totalAsset: number;
+};
+
+export type TMyPortfolio = {
+  summary: TSummary;
+  assets: TAssets[];
+};
+
+export type TAssets = {
+  categoryId: number;
+  categoryName: string;
+  symbol: string;
+  quantity: number;
+  avgBuyPrice: number;
+  buyAmount: number;
+  evaluateAmount: number;
+  profit: number;
+};
+export type TSummary = {
+  totalBuyAmount: number;
+  totalEvaluateAmount: number;
+  totalProfit: number;
+  totalProfitRate: number;
+};
