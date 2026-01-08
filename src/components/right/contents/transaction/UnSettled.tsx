d816a4fd05deeeafc6cfcb57769b55e416e97dcc
202110861 <dbsalsk2514@naver.com>
2026-01-08 22:55:46 +0900

    style: 미체결 데이터가 일정 높이를 넘으면 hidden 및 스크롤

--- src/components/right/contents/transaction/UnSettled.tsx
+++ src/components/right/contents/transaction/UnSettled.tsx
@@ -22,44 +22,46 @@ const UnSettled = () => {
           전체 취소
         </button>
       </div>
-      <table className="w-full border-collapse bg-white">
-        <thead>
-          <tr className="border-b border-t border-gray-200 bg-[#F7F7F7]">
-            <th className="py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
-              주문시간
-            </th>
-            <th className="py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
-              <div className="flex flex-col">
-                <span>마켓명</span>
-                <span className="text-[11px] font-normal text-[#666666] mt-1">구분</span>
-              </div>
-            </th>
-            <th className="py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
-              <span className="text-[11px] font-normal text-[#666666] mt-1">주문금액</span>
-            </th>
-            <th className="py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
-              <div className="flex flex-col">
-                <span>주문량</span>
-                <span className="text-[11px] font-normal text-[#666666] mt-1">미체결량</span>
-              </div>
-            </th>
-            <th className="py-3 text-center text-[11px] font-medium text-[#666666] ">주문취소</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data && data.length > 0 ? (
-            data.map((item: TUnSettledData, index: number) => (
-              <UnSettledItem item={item} index={index} refetch={refetch} />
-            ))
-          ) : (
-            <tr>
-              <td colSpan={10} className="text-[13px] text-center text-[#666666] border-b border-gray-200 py-10">
-                미체결 내역이 없습니다.
-              </td>
+      <div className="max-h-[650px] overflow-y-auto w-full border-collapse bg-white">
+        <table className="w-full border-collapse bg-white">
+          <thead className="sticky top-0 z-10">
+            <tr className="border-b border-t border-gray-200 bg-[#F7F7F7]">
+              <th className="py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
+                주문시간
+              </th>
+              <th className="py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
+                <div className="flex flex-col">
+                  <span>마켓명</span>
+                  <span className="text-[11px] font-normal text-[#666666] mt-1">구분</span>
+                </div>
+              </th>
+              <th className="py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
+                <span className="text-[11px] font-normal text-[#666666] mt-1">주문금액</span>
+              </th>
+              <th className="py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
+                <div className="flex flex-col">
+                  <span>주문량</span>
+                  <span className="text-[11px] font-normal text-[#666666] mt-1">미체결량</span>
+                </div>
+              </th>
+              <th className="py-3 text-center text-[11px] font-medium text-[#666666] ">주문취소</th>
             </tr>
-          )}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {data && data.length > 0 ? (
+              data.map((item: TUnSettledData, index: number) => (
+                <UnSettledItem item={item} index={index} refetch={refetch} />
+              ))
+            ) : (
+              <tr>
+                <td colSpan={10} className="text-[13px] text-center text-[#666666] border-b border-gray-200 py-10">
+                  미체결 내역이 없습니다.
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
