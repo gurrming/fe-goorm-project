06f3629a0f4bfa03a9698d9b03310e103aead10f
202110861 <dbsalsk2514@naver.com>
2026-01-09 09:13:35 +0900

    fix: 체결 목록 데이터 일정 개수 넘으면 hidden 하고 스크롤로 설정

--- src/components/right/contents/transaction/Settled.tsx
+++ src/components/right/contents/transaction/Settled.tsx
@@ -7,36 +7,40 @@ const Settled = () => {
   console.log('채결 내역 : ', data);
 
   return (
-    <table className="w-full border-collapse bg-white">
-      <thead>
-        <tr className="border-b border-t border-gray-200 bg-[#F7F7F7]">
-          <th className="py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">주문시간</th>
-          <th className="py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
-            <div className="flex flex-col">
-              <span>마켓명</span>
-              <span className="text-[11px] font-normal text-[#666666] mt-1">구분</span>
-            </div>
-          </th>
-          <th className="py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
-            <div className="flex flex-col">
-              <span>체결가격</span>
-            </div>
-          </th>
-          <th className="py-3 text-center text-[11px] font-medium text-[#666666]">체결수량</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data && data.length > 0 ? (
-          data.map((item: TSettledData, index: number) => <SettledItem item={item} index={index} />)
-        ) : (
-          <tr>
-            <td colSpan={10} className="text-[13px] text-center text-[#666666] border-b border-gray-200 py-10">
-              채결 내역이 없습니다.
-            </td>
+    <div className="max-h-[650px] overflow-y-auto w-full border-collapse bg-white">
+      <table className="w-full border-collapse bg-white">
+        <thead className="sticky top-0 z-10">
+          <tr className="border-b border-t border-gray-200 bg-[#F7F7F7]">
+            <th className="py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
+              주문시간
+            </th>
+            <th className="py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
+              <div className="flex flex-col">
+                <span>마켓명</span>
+                <span className="text-[11px] font-normal text-[#666666] mt-1">구분</span>
+              </div>
+            </th>
+            <th className="py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
+              <div className="flex flex-col">
+                <span>체결가격</span>
+              </div>
+            </th>
+            <th className="py-3 text-center text-[11px] font-medium text-[#666666]">체결수량</th>
           </tr>
-        )}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data && data.length > 0 ? (
+            data.map((item: TSettledData, index: number) => <SettledItem item={item} index={index} />)
+          ) : (
+            <tr>
+              <td colSpan={10} className="text-[13px] text-center text-[#666666] border-b border-gray-200 py-10">
+                채결 내역이 없습니다.
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
