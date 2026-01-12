c43c98460bca26137b1d7f4bca9cb2c56e78e04a
202110861 <dbsalsk2514@naver.com>
2026-01-12 14:11:06 +0900

    fix: get chart url에 쿼리 추가

--- src/api/useGetChart.ts
+++ src/api/useGetChart.ts
@@ -2,16 +2,16 @@ import { useQuery } from '@tanstack/react-query';
 import { request } from './common/axiosInstance';
 import type { ChartData } from '../types/websocket';
 
-const getChart = (categoryId: number): Promise<ChartData[]> => {
+const getChart = (categoryId: number, page: number, size: number): Promise<ChartData[]> => {
   return request<ChartData[]>({
     method: 'GET',
-    url: `/api/trades/chart?categoryId=${categoryId}`,
+    url: `/api/trades/chart?categoryId=${categoryId}&page=${page}&size=${size}`,
   });
 };
 
-export const useGetChart = (categoryId: number) => {
+export const useGetChart = (categoryId: number, page: number, size: number) => {
   return useQuery({
     queryKey: ['chart', categoryId],
-    queryFn: () => getChart(categoryId),
+    queryFn: () => getChart(categoryId, page, size),
   });
 };
