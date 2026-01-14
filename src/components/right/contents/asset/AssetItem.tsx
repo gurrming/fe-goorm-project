fafbd50f96fccf0f26ea47685cd2933edfc8e258
202110861 <dbsalsk2514@naver.com>
2026-01-14 14:34:44 +0900

    feat: 종목 이름, 심볼 같이 보이도록 추가

--- src/components/right/contents/asset/AssetItem.tsx
+++ src/components/right/contents/asset/AssetItem.tsx
@@ -7,7 +7,10 @@ const AssetItem = ({ item }: { item: TAssets }) => {
   };
   return (
     <tr className="border-b border-gray-200 hover:bg-gray-50">
-      <td className="px-2 py-3 text-xs text-[#333333] text-center border-r border-gray-200">{item.symbol}</td>
+      <td className="px-2 py-3 text-xs text-[#333333] text-center border-r border-gray-200 flex flex-col gap-1">
+        <p className="text-xs font-semibold">{item.categoryName}</p>
+        <p className="text-[10px] text-gray-500">{item.symbol}</p>
+      </td>
       <td className="pr-2 py-3 text-xs text-[#333333] text-right border-r border-gray-200">
         {item.investCount?.toLocaleString('ko-KR', numberOption)}
       </td>
