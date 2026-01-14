400452b171c88fdb9f15ff68a1d6d34787be8aaf
202110861 <dbsalsk2514@naver.com>
2026-01-14 14:08:41 +0900

    fix: type 이름 변경에 따른 코드 수정

--- src/components/right/contents/asset/AssetItem.tsx
+++ src/components/right/contents/asset/AssetItem.tsx
@@ -1,26 +1,29 @@
 import type { TAssets } from '../../../../types/asset';
 
 const AssetItem = ({ item }: { item: TAssets }) => {
+  const numberOption = {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  };
   return (
     <tr className="border-b border-gray-200 hover:bg-gray-50">
-      <td className="px-2 py-3 text-xs text-[#333333] text-center border-r border-gray-200">{item.categoryName}</td>
+      <td className="px-2 py-3 text-xs text-[#333333] text-center border-r border-gray-200">{item.symbol}</td>
       <td className="pr-2 py-3 text-xs text-[#333333] text-right border-r border-gray-200">
-        {item.quantity.toLocaleString('ko-KR', {
-          minimumFractionDigits: 2,
-          maximumFractionDigits: 8,
-        })}
+        {item.investCount?.toLocaleString('ko-KR', numberOption)}
       </td>
       <td className="pr-2 py-3 text-xs text-[#333333] text-right border-r border-gray-200">
-        {item.avgBuyPrice.toLocaleString('ko-KR')}
+        {item.avgPrice?.toLocaleString('ko-KR', numberOption)}
       </td>
       <td className="pr-2 py-3 text-xs text-[#333333] text-right border-r border-gray-200">
-        {item.buyAmount.toLocaleString('ko-KR')}
+        {item.buyAmount?.toLocaleString('ko-KR', numberOption)}
       </td>
       <td className="pr-2 py-3 text-xs text-[#333333] text-right border-r border-gray-200">
-        {item.evaluateAmount.toLocaleString('ko-KR')}
+        {item.evaluationAmount?.toLocaleString('ko-KR', numberOption)}
       </td>
-      <td className="pr-2 py-3 text-xs text-[#333333] text-right border-r border-gray-200">
-        {item.profit.toLocaleString('ko-KR')}
+      <td
+        className={`pr-2 py-3 text-xs text-[#333333] text-right border-r border-gray-200 ${item.evaluationProfit && item.evaluationProfit > 0 ? 'text-red-500' : 'text-blue-500'}`}
+      >
+        {item.evaluationProfit?.toLocaleString('ko-KR', numberOption)}
       </td>
     </tr>
   );
