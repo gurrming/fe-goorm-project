2a0617810faec3d9fd9a51d11c3d892f7f2fae76
202110861 <dbsalsk2514@naver.com>
2026-01-15 14:14:10 +0900

    style: text 관련 스타일 변경

--- src/components/asset/AssetList.tsx
+++ src/components/asset/AssetList.tsx
@@ -13,26 +13,26 @@ const AssetList = () => {
 
   return (
     <div className="flex flex-col gap-3 border-t-[0.3px] border-gray-200 pt-3">
-      <p className="text-[15px] text-[#333333] px-4">보유자산 목록</p>
+      <p className="text-[15px] text-[#333333] font-bold px-4">보유자산 목록</p>
       <table className="w-full border-collapse bg-white text-nowrap">
         <thead>
           <tr className="border-b border-t border-gray-200 bg-[#F7F7F7]">
-            <th className=" py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
+            <th className=" py-2 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
               보유자산
             </th>
-            <th className=" py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
+            <th className=" py-2 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
               보유수량
             </th>
-            <th className=" py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
+            <th className=" py-2 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
               매수평균가
             </th>
-            <th className=" py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
+            <th className=" py-2 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
               매수금액
             </th>
-            <th className=" py-3 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
+            <th className=" py-2 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
               평가금액
             </th>
-            <th className=" py-3 text-center text-[11px] font-medium text-[#666666]">평가손익</th>
+            <th className=" py-2 text-center text-[11px] font-medium text-[#666666]">평가손익</th>
           </tr>
         </thead>
         <tbody className="max-h-[300px] overflow-hidden">
