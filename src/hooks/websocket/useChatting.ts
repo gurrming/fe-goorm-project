e11febd3c4b266b9da2e86e47713c46a1896558a
202110861 <dbsalsk2514@naver.com>
2026-01-22 09:32:51 +0900

    fix: 웹소켓 외의 관련 없는 코드 삭제

--- src/hooks/websocket/useChatting.ts
+++ src/hooks/websocket/useChatting.ts
@@ -1,5 +1,4 @@
 import { useCallback, useEffect, useState } from 'react';
-import { useGetChatList } from '../../api/useGetChatList';
 import useUserStore from '../../store/useUserStore';
 import { useWebsocket } from '../useWebsocket';
 import type { TChat } from '../../types/chat';
@@ -7,18 +6,11 @@ import type { TChat } from '../../types/chat';
 export const useChatting = ({ categoryId }: { categoryId: number }) => {
   const { isConnected, stompClientRef } = useWebsocket();
   const [chatHistory, setChatHistory] = useState<TChat[]>([]);
-  const { data: chatList, refetch } = useGetChatList(categoryId);
   const user = useUserStore((state) => state.user);
 
   useEffect(() => {
-    if (chatList) {
-      setChatHistory(chatList);
-    }
-  }, [chatList]);
-
-  useEffect(() => {
-    refetch();
-  }, [categoryId, refetch]);
+    setChatHistory([]);
+  }, [categoryId]);
 
   useEffect(() => {
     if (isConnected && stompClientRef.current && categoryId) {
