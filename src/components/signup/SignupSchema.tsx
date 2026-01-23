08cf40b9a5889394e460d2f492b91193298607d5
202110861 <dbsalsk2514@naver.com>
2026-01-23 15:13:21 +0900

    fix: 이메일 중복 확인 관련 메세지 조건 추가 및 비밀번호 확인 유효성 검사 순서 변경

--- src/components/signup/SignupSchema.tsx
+++ src/components/signup/SignupSchema.tsx
@@ -5,14 +5,24 @@ const signupSchema = yup.object().shape({
   emailExist: yup
     .boolean()
     .nullable()
-    .test('is-checked', '이메일 중복 확인을 해주세요.', (value) => value !== null && value !== undefined)
-    .oneOf([false], '이미 사용 중인 이메일 입니다.'),
-  password: yup.string().min(8, '비밀번호는 8자 이상이어야 합니다.').required('비밀번호는 필수 입력 사항입니다.'),
+    .when('email', {
+      is: (email: string) => yup.string().email().required().isValidSync(email),
+      then: (schema) =>
+        schema
+          .test('is-checked', '이메일 중복 확인을 해주세요.', (value) => value !== null && value !== undefined)
+          .oneOf([false], '이미 사용 중인 이메일 입니다.'),
+      otherwise: (schema) => schema.nullable(),
+    }),
+    
+  password: yup.string().required('비밀번호는 필수 입력 사항입니다.').min(8, '비밀번호는 8자 이상이어야 합니다.'),
   passwordConfirm: yup
     .string()
-    .oneOf([yup.ref('password')], '비밀번호가 일치하지 않습니다.')
-    .required('비밀번호 확인은 필수 입력 사항입니다.'),
-  nickname: yup.string().required('닉네임은 필수 입력 사항입니다.'),
+    .required('비밀번호 확인은 필수 입력 사항입니다.')
+    .test('password-match', '비밀번호가 일치하지 않습니다.', function (value){
+      if(!value) return true;
+      return value === this.parent.password;
+    }),
+  nickname: yup.string().required('닉네임은 필수 입력 사항입니다.').min(2, '닉네임은 2자 이상이어야 합니다.'),
 });
 
 export default signupSchema;
