bd2f87392e799e7d4ffc72611655a93dda069ed0
202110861 <dbsalsk2514@naver.com>
2026-01-23 13:58:20 +0900

    feat: 미체결 응답 데이터형식 추가

--- src/types/transaction.ts
+++ src/types/transaction.ts
@@ -28,3 +28,25 @@ export type TUnSettledData = {
   totalAmount: number;
   orderTime: string;
 };
+
+export type TUnSettledResponse = {
+  orders: {
+    content: TUnSettledData[];
+    last: boolean;      // 마지막 페이지 여부
+    number: number;     // 현재 페이지 번호
+    pageable:{
+      offset: number;
+      pageSize: number; // 전체 페이지 수
+      pageNumber: number; // 현재 페이지 번호
+      paged: boolean;
+      sort: {
+        sorted: boolean;
+        unsorted: boolean;
+        empty: boolean;
+      };
+      unpaged: boolean;
+    };
+    size: number;
+  };
+  totalOpenOrderCount: number;
+};
