9487fb43a9bdf1d67e8ac5fbb53577bbb188c86a
202110861 <dbsalsk2514@naver.com>
2026-01-23 13:58:59 +0900

    fix: 추가된 데이터 형식에 따른 타입 변경

--- src/api/transaction/useGetUnSettledData.ts
+++ src/api/transaction/useGetUnSettledData.ts
@@ -1,21 +1,21 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, type UseQueryResult } from '@tanstack/react-query';
 import useUserStore from '../../store/useUserStore';
 import { request } from '../common/axiosInstance';
-import type { TUnSettledData } from '../../types/transaction';
+import type { TUnSettledResponse } from '../../types/transaction';
 
 export const getUnSettledData = (memberId: number, page: number, size: number)=> {
-  return request<TUnSettledData[]>({
+  return request<TUnSettledResponse>({
     method: 'GET',
     url: `/api/orders/open?memberId=${memberId}&page=${page}&size=${size}`,
   });
 };
 
-export const useGetUnSettledData = (page: number, size: number) => {
+export const useGetUnSettledData = (page: number, size: number): UseQueryResult<TUnSettledResponse, Error> => {
   const user = useUserStore((state) => state.user);
   const memberId = user?.id;
   const isLoggedIn = !!user;
 
-  return useQuery({
+  return useQuery<TUnSettledResponse, Error>({
     queryKey: ['unsettled'],
     queryFn: () => getUnSettledData(memberId!, page, size),
     enabled: isLoggedIn,
