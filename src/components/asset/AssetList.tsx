b2ffdae79469685579b330dbba2fa3f14a758a8d
202110861 <dbsalsk2514@naver.com>
2026-01-23 17:46:53 +0900

    style: 보유자산 목록 컴포넌트 스타일 변경(세로 길이)

--- src/components/asset/AssetList.tsx
+++ src/components/asset/AssetList.tsx
@@ -12,10 +12,10 @@ const AssetList = () => {
   useSummary(memberId!);
 
   return (
-    <div className="flex flex-col gap-3 border-t-[0.3px] border-gray-200 pt-3">
+    <div className="h-[600px] overflow-y-auto flex flex-col gap-3 border-t-[0.3px] border-gray-200 pt-3">
       <p className="text-[15px] text-[#333333] font-bold px-4">보유자산 목록</p>
       <table className="w-full border-collapse bg-white text-nowrap">
-        <thead>
+        <thead className="sticky top-0 z-10 border-b border-gray-200">
           <tr className="border-b border-t border-gray-200 bg-[#F7F7F7]">
             <th className=" py-2 text-center text-[11px] font-medium text-[#666666] border-r border-gray-200">
               보유자산
@@ -35,7 +35,7 @@ const AssetList = () => {
             <th className=" py-2 text-center text-[11px] font-medium text-[#666666]">평가손익</th>
           </tr>
         </thead>
-        <tbody className="h-[600px] overflow-hidden">
+        <tbody>
           {summary?.assetList && summary?.assetList.length > 0 ? (
             summary?.assetList.map((item: TAssets) => <AssetItem key={item.symbol} item={item} />)
           ) : (
