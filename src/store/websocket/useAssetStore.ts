0d5423d840f74557ddcbb5eecc57fc526b973fe7
202110861 <dbsalsk2514@naver.com>
2026-01-26 10:29:08 +0900

    fix: 자산 타입 추가에 따른 store 구조 변경

--- src/store/websocket/useAssetStore.ts
+++ src/store/websocket/useAssetStore.ts
@@ -2,23 +2,27 @@ import { create } from 'zustand';
 import type { TMyPortfolio } from '../../types/asset';
 
 interface IAssetStore {
-  assetCash: number | null;
+  myAsset:{
+    assetCash: number | null;
   totalAsset: number | null;
+  assetCanOrder: number | null;
+  }
   wsTotalAsset: number | null;
   summary: TMyPortfolio | null;
-  setAssetCash: (data: number | null) => void;
-  setTotalAsset: (data: number | null) => void;
+  setMyAsset: (data: { assetCash: number | null; totalAsset: number | null; assetCanOrder: number | null }) => void;
   setWsTotalAsset: (data: number | null) => void;
   setSummary: (data: TMyPortfolio) => void;
 }
 
 export const useAssetStore = create<IAssetStore>((set) => ({
-  assetCash: null,
-  totalAsset: null,
+  myAsset: {
+    assetCash: null,
+    totalAsset: null,
+    assetCanOrder: null,
+  },
   wsTotalAsset: null,
   summary: null,
-  setAssetCash: (data) => set({ assetCash: data }),
-  setTotalAsset: (data) => set({ totalAsset: data }),
+  setMyAsset: (data) => set({ myAsset: data }),
   setWsTotalAsset: (data) => set({ wsTotalAsset: data }),
   setSummary: (data) => set({ summary: data }),
 }));
