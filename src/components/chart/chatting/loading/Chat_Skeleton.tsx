e17509e9d51efe14153e0f6ed9eef5a6e49f1d74
202110861 <dbsalsk2514@naver.com>
2026-01-28 13:03:45 +0900

    fix: SkeletonThema 추가에 따른 수정

--- src/components/chart/chatting/loading/Chat_Skeleton.tsx
+++ src/components/chart/chatting/loading/Chat_Skeleton.tsx
@@ -4,8 +4,8 @@ import 'react-loading-skeleton/dist/skeleton.css';
 export const Chat_Skeleton = ({ width}: { width: number}) => {
     return (
         <div className="flex flex-col gap-1 mb-3">
-            <Skeleton count={1} width={70} height={15} borderRadius={5} baseColor="#f0f0f0" highlightColor="#e0e0e0" duration={1} enableAnimation={true}/>
-            <Skeleton count={1} width={width} height={35} borderRadius={5} baseColor="#f0f0f0" highlightColor="#e0e0e0" duration={1} enableAnimation={true}/>
+            <Skeleton count={1} width={70} height={15} borderRadius={5} />
+            <Skeleton count={1} width={width} height={35} borderRadius={5} />
         </div>
     )
 }
