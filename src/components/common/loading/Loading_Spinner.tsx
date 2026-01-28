8d48ff70b169ffa3abb08186e020ee7236586135
202110861 <dbsalsk2514@naver.com>
2026-01-28 13:25:22 +0900

    fix: 이름 변경 및 위치 변경

--- src/components/common/loading/Loading_Spinner.tsx
+++ src/components/common/loading/Loading_Spinner.tsx
@@ -1,6 +1,6 @@
 import { ClipLoader } from "react-spinners";
 
-export const Transaction_Spinner = () => {
+export const Loading_Spinner = () => {
     return (
         <div className="flex flex-col gap-3  items-center justify-center">
             <ClipLoader color="#0C3887" size={25} />
