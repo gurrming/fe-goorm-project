580d9f5cabe2c01d3e16716ce46303348a00765a
202110861 <dbsalsk2514@naver.com>
2026-02-02 14:01:43 +0900

    fix: numberOption, toLocalString 삭제

--- src/components/asset/AssetItem.tsx
+++ src/components/asset/AssetItem.tsx
@@ -2,10 +2,6 @@ import { formatInteger } from '../../lib/price';
 import type { TAssets } from '../../types/asset';
 
 const AssetItem = ({ item }: { item: TAssets }) => {
-  const numberOption = {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  };
   return (
     <tr className="border-b border-gray-200 hover:bg-gray-50">
       <td className="px-2 py-3 text-xs text-[#333333] text-center border-r border-gray-200 flex flex-col gap-1">
@@ -13,7 +9,7 @@ const AssetItem = ({ item }: { item: TAssets }) => {
         <p className="text-[10px] text-gray-500">{item.symbol}</p>
       </td>
       <td className="pr-2 py-3 text-xs text-[#333333] text-right border-r border-gray-200">
-        {item.investCount?.toLocaleString('ko-KR', numberOption)}
+        {item.investCount}
       </td>
       <td className="pr-2 py-3 text-xs text-[#333333] text-right border-r border-gray-200">
         {formatInteger(item.avgPrice)}
