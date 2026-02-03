a02aaab4e2dc2f76e7031771a9016f77e9313416
202110861 <dbsalsk2514@naver.com>
2026-02-03 15:52:56 +0900

    feat: storybook에 QueryClientProvider 추가

---
+++ .storybook/preview.tsx
@@ -0,0 +1,33 @@
+import type { Preview } from '@storybook/react-vite';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
+import '../src/index.css';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      gcTime: 0,
+    },
+  },
+});
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <QueryClientProvider client={queryClient}>
+        <Story />
+      </QueryClientProvider>
+    ),
+  ],
+};
+
+export default preview;
