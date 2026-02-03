680eee161efbe82f1b5924a1f479a241c855e2c7
202110861 <dbsalsk2514@naver.com>
2026-02-03 16:04:30 +0900

    feat: UnSettledItem.stories.tsx에 심볼 데이터 추가

--- src/components/stories/UnSettledItem.stories.tsx
+++ src/components/stories/UnSettledItem.stories.tsx
@@ -17,13 +17,13 @@ export default {
 export const BuyItem = {
     name: '매수 아이템',
     args: {
-        item: { orderId: 1, orderTime: '2024-01-01T12:00:00', orderPrice: 10000, orderCount: 10, remainingCount: 10, orderType: 'BUY' },
+        item: { orderId: 1, orderTime: '2024-01-01T12:00:00', orderPrice: 10000, orderCount: 10, remainingCount: 10, orderType: 'BUY', symbol: 'BTC' },
     },
 }
 
 export const SellItem = {
     name: '매도 아이템',
     args: {
-        item: { orderId: 1, orderTime: '2024-01-01T12:00:00', orderPrice: 10000, orderCount: 10, remainingCount: 10, orderType: 'SELL' },
+        item: { orderId: 1, orderTime: '2024-01-01T12:00:00', orderPrice: 10000, orderCount: 10, remainingCount: 10, orderType: 'SELL', symbol: 'BTC' },
     },
 }
