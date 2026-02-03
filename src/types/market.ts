b204c3fcf3880a637032c43ce0f05eee75e53b28
정찬영 <q135794268020@gmail.com>
2026-02-03 15:45:32 +0900

    fix: lastPrice명이 어색하여 현재가에 알맞는 nowPrice로 수정

--- src/types/market.ts
+++ src/types/market.ts
@@ -1,5 +1,5 @@
 export type TabKey = 'krw' | 'holding' | 'interest';
 
-export type SortTable = 'lastPrice' | 'changeRate' | 'tradeAmount' | 'evaluateAmount' | 'avgBuyPrice' | 'profitRate';
+export type SortTable = 'nowPrice' | 'changeRate' | 'tradeAmount' | 'evaluateAmount' | 'avgBuyPrice' | 'profitRate';
 
 export type SortPriceArray = 'descending' | 'ascending' | 'base';
