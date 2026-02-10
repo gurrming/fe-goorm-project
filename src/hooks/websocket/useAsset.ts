4534be8be56cfe817d512c662e1ba2a040f8ef7f
202110861 <dbsalsk2514@naver.com>
2026-02-10 17:27:49 +0900

    feat: 보유 종목 실시간 데이터의 필요한 부분만 저장

--- src/hooks/websocket/useAsset.ts
+++ src/hooks/websocket/useAsset.ts
@@ -1,8 +1,9 @@
 import { useEffect } from 'react';
 import { useAssetStore } from '../../store/websocket/useAssetStore';
 import { useWebsocket } from '../useWebsocket';
-import type { TWSAssets, TMyPortfolio } from '../../types/asset';
+import type { TMyPortfolio } from '../../types/asset';
 
+/*
 export const useAsset = (memberId: number) => {
   const { isConnected, stompClientRef } = useWebsocket();
   const { setWsTotalAsset } = useAssetStore();
@@ -12,6 +13,7 @@ export const useAsset = (memberId: number) => {
       const subscription = stompClientRef.current.subscribe(`/topic/assets/${memberId}`, (message) => {
         try {
           const data: TWSAssets = JSON.parse(message.body);
+          console.log('웹소켓 assets data', data);
           setWsTotalAsset(data.totalAsset);
         } catch (error) {
           console.error('[useAsset] 데이터 파싱 에러:', error);
@@ -23,16 +25,23 @@ export const useAsset = (memberId: number) => {
     }
   }, [isConnected, memberId, setWsTotalAsset, stompClientRef]);
 };
+*/
 
 export const useSummary = (memberId: number) => {
   const { isConnected, stompClientRef } = useWebsocket();
-  const { setSummary } = useAssetStore();
+  const { setSummary, setWsAssetList } = useAssetStore();
 
   useEffect(() => {
     if (isConnected && stompClientRef.current && memberId) {
       const subscription = stompClientRef.current.subscribe(`/topic/invest/${memberId}`, (message) => {
         try {
           const data: TMyPortfolio = JSON.parse(message.body);
+          const wsAssetList = data.assetList.map((item) => ({
+            categoryId: item.categoryId,
+            evaluationAmount: item.evaluationAmount,
+            evaluationProfit: item.evaluationProfit,
+          }));
+          setWsAssetList(wsAssetList);
           setSummary(data);
         } catch (error) {
           console.error('[useSummary] 데이터 파싱 에러:', error);
@@ -42,5 +51,5 @@ export const useSummary = (memberId: number) => {
         subscription.unsubscribe();
       };
     }
-  }, [isConnected, memberId, setSummary, stompClientRef]);
+  }, [isConnected, memberId, setSummary, setWsAssetList, stompClientRef]);
 };
