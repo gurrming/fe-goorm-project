0943dd5d8b06fd397d601c195b2e1a585a7bc965
202110861 <dbsalsk2514@naver.com>
2026-02-18 13:18:57 +0900

    feat: Gzip 압축 활성화

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,18 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { visualizer } from 'rollup-plugin-visualizer';
+import viteCompression from 'vite-plugin-compression';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    viteCompression({
+      algorithm: 'gzip', // 'brotliCompress'로 변경 가능 (서버 지원 시)
+      ext: '.gz',
+      threshold: 10240, // 10kb 이상만 압축
+    }),
     process.env.BUNDLE_ANALYZE === '1' &&
       visualizer({
         open: true,
